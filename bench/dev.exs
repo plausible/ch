56cdfdd3d7c915fbf43b6092cce23e6bad188b1f
ruslandoga <67764432+ruslandoga@users.noreply.github.com>
2022-12-12 16:35:23 +0700

    remove dev bench

--- bench/dev.exs
+++ bench/dev.exs
@@ -1,9 +1,4 @@
 Benchee.run(
-  %{
-    "nullable" => fn -> Ch.Protocol.encode(:u16, 1) end,
-    "nullable nil" => fn -> Ch.Protocol.encode(:u16, nil) end,
-    "non-nullable" => fn -> Ch.Protocol.encode(:i16, 1) end,
-    "non-nullable nil" => fn -> Ch.Protocol.encode(:i16, nil || 0) end
-  },
+  %{},
   memory_time: 2
 )
