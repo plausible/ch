1dc70f2bdee197f4774514d923cd65e45b6d918c
ruslandoga <rusl@n-do.ga>
2023-03-13 15:25:12 +0700

    improve extract_command/1 (#19)

--- test/ch/query_test.exs
+++ test/ch/query_test.exs
@@ -1,8 +1,40 @@
 defmodule Ch.QueryTest do
   use ExUnit.Case, async: true
+  alias Ch.Query
 
   test "to_string" do
-    query = Ch.Query.build(["select ", 1 + ?0, ?+, 2 + ?0])
+    query = Query.build(["select ", 1 + ?0, ?+, 2 + ?0])
     assert to_string(query) == "select 1+2"
+  end
+
+  describe "command" do
+    test "without command provided" do
+      assert Query.build("select 1+2").command == :select
+      assert Query.build("select 1+2").command == :select
+      assert Query.build("   select 1+2").command == :select
+      assert Query.build("\t\n\t\nselect 1+2").command == :select
+
+      assert Query.build("""
+
+             select 1+2
+             """).command == :select
+
+      assert Query.build(["select 1+2"]).command == :select
+
+      assert Query.build("with insert as (select 1) select * from insert").command == :select
+    end
+
+    test "with nil command provided" do
+      assert Query.build("select 1+2", nil).command == :select
+    end
+
+    test "with command provided" do
+      assert Query.build("select 1+2", :custom).command == :custom
+    end
+
+    @tag skip: true
+    test "TODO" do
+      assert Query.build("Select 1+2").command == :select
+    end
   end
 end
