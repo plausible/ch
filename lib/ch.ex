8ac71beb361fe623e7a92f4aa640a5b004da037c
ruslandoga <67764432+ruslandoga@users.noreply.github.com>
2023-04-12 17:10:50 +0700

    docs

--- lib/ch.ex
+++ lib/ch.ex
@@ -2,16 +2,48 @@ defmodule Ch do
   @moduledoc "Minimal HTTP ClickHouse client"
   alias Ch.{Connection, Query, Result}
 
+  @doc """
+  Start the connection process and connect to ClickHouse.
+
+  ## Options
+
+    * `:hostname` - server hostname, defaults to `localhost`
+    * `:port` - HTTP port, defualts to `8123`
+    * `:scheme` - HTTP scheme, defaults to `"http"`
+    * `:database` - Database, defaults to `"default"`
+    * `:username` - Username
+    * `:password` - User password
+    * `:settings` - Keyword list of settings
+    * `:timeout` - HTTP receive timeout in milliseconds
+
+  """
   def start_link(opts \\ []) do
     DBConnection.start_link(Connection, opts)
   end
 
+  @doc """
+  Returns a supervisor child specification for a DBConnection pool.
+  """
   def child_spec(opts) do
     DBConnection.child_spec(Connection, opts)
   end
 
-  @spec query(DBConnection.conn(), iodata, {:raw, iodata} | Enumerable.t(), Keyword.t()) ::
+  @doc """
+  Runs a query and returns the result as `{:ok, %Ch.Result{}}` or
+  `{:error, %Ch.Error{}}` if there was a database error.
+
+  ## Options
+
+    * `:timeout` - Query request timeout
+    * `:settings` - Keyword list of settings
+    * `:database` - Database
+    * `:username` - Username
+    * `:password` - User password
+
+  """
+  @spec query(DBConnection.conn(), iodata, params, Keyword.t()) ::
           {:ok, Result.t()} | {:error, Exception.t()}
+        when raw: iodata | Enumerable.t(), params: map | [term] | [row :: [term]] | {:raw, raw}
   def query(conn, statement, params \\ [], opts \\ []) do
     query = Query.build(statement, Keyword.get(opts, :command))
 
@@ -20,15 +52,19 @@ defmodule Ch do
     end
   end
 
-  @spec query!(DBConnection.conn(), iodata, {:raw, iodata} | Enumerable.t(), Keyword.t()) ::
-          Result.t()
+  @doc """
+  Runs a query and returns the result or raises `Ch.Error` if
+  there was an error. See `query/4`.
+  """
+  @spec query!(DBConnection.conn(), iodata, params, Keyword.t()) :: Result.t()
+        when raw: iodata | Enumerable.t(), params: map | [term] | [row :: [term]] | {:raw, raw}
   def query!(conn, statement, params \\ [], opts \\ []) do
     query = Query.build(statement, Keyword.get(opts, :command))
     DBConnection.execute!(conn, query, params, opts)
   end
 
   @doc false
-  @spec stream(DBConnection.t(), iodata, list, Keyword.t()) :: DBConnection.Stream.t()
+  @spec stream(DBConnection.t(), iodata, map | [term], Keyword.t()) :: DBConnection.Stream.t()
   def stream(conn, statement, params \\ [], opts \\ []) do
     query = Query.build(statement, Keyword.get(opts, :command))
     DBConnection.stream(conn, query, params, opts)
