8430f186115092ebd49dcadd6cca36dc3098f597
Dario Heinisch <dario.heinisch@gmail.com>
2023-04-24 12:45:44 +0200

    fix

--- lib/ch/query.ex
+++ lib/ch/query.ex
@@ -235,21 +235,15 @@ defimpl DBConnection.Query, for: Ch.Query do
 
   defp encode_map_params([] = empty), do: empty
 
-  defp encode_array_param(s) when is_binary(s) do
+  defp encode_array_param(i) when is_integer(i), do: encode_param(i)
+  defp encode_array_param(f) when is_float(f), do: encode_param(f)
+  defp encode_array_param(xs) when is_list(xs), do: encode_param(xs)
+  defp encode_array_param(xs) when is_tuple(xs), do: encode_param(xs)
+
+  defp encode_array_param(s) do
+    s = encode_param(s)
     [?', to_iodata(s, 0, s, []), ?']
   end
-
-  defp encode_array_param(v), do: encode_param(v)
-
-  # defp encode_array_param(i) when is_integer(i), do: encode_param(i)
-  # defp encode_array_param(f) when is_float(f), do: encode_param(f)
-  # defp encode_array_param(xs) when is_list(xs), do: encode_param(xs)
-  # defp encode_array_param(xs) when is_tuple(xs), do: encode_param(xs)
-
-  # defp encode_array_param(s) do
-  #   s = encode_param(s)
-  #   [?', to_iodata(s, 0, s, []), ?']
-  # end
 
   defp encode_map_param({k, v}) do
     [encode_array_param(k), ?:, encode_array_param(v)]
