8197f1f89fd6c276dc92638089ecb9d03c4685d4
ruslandoga <rusl@n-do.ga>
2023-06-01 18:02:39 +0800

    some aggregate function tests (#92)

--- test/ch/aggregation_test.exs
+++ test/ch/aggregation_test.exs
@@ -3,9 +3,12 @@ defmodule Ch.AggregationTest do
 
   setup do
     conn = start_supervised!({Ch, database: Ch.Test.database()})
+    {:ok, conn: conn}
+  end
 
-    create_table = """
-    create table candle_fragments (
+  test "select SimpleAggregateFunction types", %{conn: conn} do
+    Ch.query!(conn, """
+    CREATE TABLE candle_fragments (
       ticker LowCardinality(String),
       time DateTime('UTC') CODEC(Delta, Default),
       high Float64 CODEC(Delta, Default),
@@ -14,11 +17,9 @@ defmodule Ch.AggregationTest do
       low  Float64 CODEC(Delta, Default),
     ) ENGINE = MergeTree()
     ORDER BY (ticker, time)
-    """
+    """)
 
-    Ch.query!(conn, create_table)
-
-    create_table = """
+    Ch.query!(conn, """
     CREATE MATERIALIZED VIEW candles_one_hour_amt
     (
       ticker LowCardinality(String),
@@ -31,64 +32,83 @@ defmodule Ch.AggregationTest do
     ENGINE = AggregatingMergeTree()
     ORDER BY (ticker, time)
     AS
-    select
-    t.ticker as ticker,
-    toStartOfHour(t.time) as time,
-    max(t.high) as high,
-    argMinState(t.open, t.time) as open,
-    argMaxState(t.close, t.time) as close,
-    min(t.low) as low
-    from candle_fragments t
-    group by ticker, time
-    """
+    SELECT
+      t.ticker AS ticker,
+      toStartOfHour(t.time) AS time,
+      max(t.high) AS high,
+      argMinState(t.open, t.time) AS open,
+      argMaxState(t.close, t.time) AS close,
+      min(t.low) AS low
+    FROM candle_fragments t
+    GROUP BY ticker, time
+    """)
 
-    Ch.query!(conn, create_table)
+    Ch.query!(conn, """
+    INSERT INTO candle_fragments(ticker, time, high, open, close, low) VALUES
+    ('INTC', '2023-04-13 20:33:00', 32, 32, 32, 32),
+    ('INTC', '2023-04-13 20:34:00', 33, 33, 33, 33),
+    ('INTC', '2023-04-13 20:35:00', 32, 32, 31, 26),
+    ('INTC', '2023-04-13 20:36:00', 32, 27, 27, 27)
+    """)
 
-    insert_query = """
-      insert into candle_fragments
-        (ticker, time, high, open, close, low)
-      VALUES
-      ('INTC', '2023-04-13 20:33:00', 32, 32, 32, 32),
-      ('INTC', '2023-04-13 20:34:00', 33, 33, 33, 33),
-      ('INTC', '2023-04-13 20:35:00', 32, 32, 31, 26),
-      ('INTC', '2023-04-13 20:36:00', 32, 27, 27, 27)
-    """
-
-    Ch.query!(conn, insert_query)
-
-    {:ok, conn: conn}
+    assert Ch.query!(conn, """
+           SELECT
+             t.ticker AS ticker,
+             toStartOfHour(t.time) AS start_time,
+             toStartOfHour(t.time) + interval 1 hour AS end_time,
+             toStartOfHour(t.time)::DATE AS date,
+             max(t.high) AS high,
+             argMinMerge(t.open) AS open,
+             argMaxMerge(t.close) AS close,
+             min(t.low) AS low
+           FROM candles_one_hour_amt t
+           GROUP BY ticker, time
+           """).rows == [
+             [
+               "INTC",
+               ~U[2023-04-13 20:00:00Z],
+               ~U[2023-04-13 21:00:00Z],
+               ~D[2023-04-13],
+               33.0,
+               32.0,
+               27.0,
+               26.0
+             ]
+           ]
   end
 
-  test "can decode aggregation types", %{conn: conn} do
-    query = """
-    select
-    t.ticker as ticker,
-    toStartOfHour(t.time) as start_time,
-    toStartOfHour(t.time) + interval 1 hour as end_time,
-    toStartOfHour(t.time)::DATE as date,
-    max(t.high) as high,
-    argMinMerge(t.open) as open,
-    argMaxMerge(t.close) as close,
-    min(t.low) as low
-    from candles_one_hour_amt t
-    group by ticker, time
-    """
+  # based on https://github.com/ClickHouse/clickhouse-java/issues/1232
+  test "insert AggregateFunction via input()", %{conn: conn} do
+    Ch.query!(conn, """
+    CREATE TABLE test_insert_aggregate_function (
+      uid Int16,
+      updated SimpleAggregateFunction(max, DateTime),
+      name AggregateFunction(argMax, String, DateTime)
+    ) ENGINE AggregatingMergeTree ORDER BY uid
+    """)
 
-    %{rows: rows} = Ch.query!(conn, query)
-
-    expected = [
-      [
-        "INTC",
-        ~U[2023-04-13 20:00:00Z],
-        ~U[2023-04-13 21:00:00Z],
-        ~D[2023-04-13],
-        33.0,
-        32.0,
-        27.0,
-        26.0
-      ]
+    rows = [
+      [1, ~N[2020-01-02 00:00:00], "b"],
+      [1, ~N[2020-01-01 00:00:00], "a"]
     ]
 
-    assert rows == expected
+    assert %{num_rows: 2} =
+             Ch.query!(
+               conn,
+               """
+               INSERT INTO test_insert_aggregate_function
+                 SELECT uid, updated, arrayReduce('argMaxState', [name], [updated])
+                 FROM input('uid Int16, updated DateTime, name String')
+                 FORMAT RowBinary\
+               """,
+               rows,
+               types: ["Int16", "DateTime", "String"]
+             )
+
+    assert Ch.query!(conn, """
+           SELECT uid, max(updated) AS updated, argMaxMerge(name)
+           FROM test_insert_aggregate_function
+           GROUP BY uid
+           """).rows == [[1, ~N[2020-01-02 00:00:00], "b"]]
   end
 end
