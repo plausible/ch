06d7e1271debedee9d92aa676b0074e6f16f1b7e
ruslandoga <67764432+ruslandoga@users.noreply.github.com>
2024-01-11 13:25:11 +0900

    refactor Ch.stream/4

--- bench/stream.exs
+++ bench/stream.exs
@@ -10,36 +10,30 @@ statement = fn limit ->
   "SELECT number FROM system.numbers_mt LIMIT #{limit}"
 end
 
-run_stream = fn statement, opts ->
-  f = fn conn -> conn |> Ch.stream(statement, [], opts) |> Stream.run() end
-  Ch.run(conn, f, timeout: :infinity)
-end
-
 Benchee.run(
   %{
-    "stream without decode" => fn statement ->
-      run_stream.(statement, _opts = [])
+    "RowBinary stream without decode" => fn statement ->
+      DBConnection.run(
+        conn,
+        fn conn ->
+          conn
+          |> Ch.stream(statement, _params = [], format: "RowBinary")
+          |> Stream.run()
+        end,
+        timeout: :infinity
+      )
     end,
-    # TODO why is this faster?
-    "stream with manual decode" => fn statement ->
-      f = fn conn ->
+    "RowBinary stream with manual decode" => fn statement ->
+      DBConnection.run(conn, fn conn ->
         conn
-        |> Ch.stream(statement, [], format: "RowBinary")
-        |> Stream.map(fn responses ->
-          Enum.each(responses, fn
-            {:data, _ref, data} -> Ch.RowBinary.decode_rows(data, [:u64])
-            {:status, _ref, 200} -> :ok
-            {:headers, _ref, _headers} -> :ok
-            {:done, _ref} -> :ok
-          end)
+        |> Ch.stream(statement, _params = [], format: "RowBinary")
+        |> Stream.map(fn %Ch.Result{data: data} ->
+          data
+          |> IO.iodata_to_binary()
+          |> Ch.RowBinary.decode_rows([:u64])
         end)
         |> Stream.run()
-      end
-
-      Ch.run(conn, f, timeout: :infinity)
-    end,
-    "stream with decode" => fn statement ->
-      run_stream.(statement, types: [:u64])
+      end)
     end
   },
   inputs: %{
