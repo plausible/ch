9f0a329edffa1d5c72b70639ae12fc89f7c74a7b
ruslandoga <67764432+ruslandoga@users.noreply.github.com>
2024-10-31 17:26:26 +0700

    add failing test

---
+++ test/ch/connect_test.exs
@@ -0,0 +1,12 @@
+defmodule Ch.ConnectTest do
+  use ExUnit.Case, async: true
+
+  test "retries to connect even with exceptions" do
+    {:ok, conn} =
+      Ch.start_link(database: Ch.Test.database(), transport_opts: [sndbuf: nil])
+
+    :timer.sleep(:timer.seconds(5))
+
+    assert Process.alive?(conn)
+  end
+end
