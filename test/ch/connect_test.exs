97430304618c3f2f9893b2cff9440bcafe896b0a
ruslandoga <67764432+ruslandoga@users.noreply.github.com>
2024-10-31 17:26:38 +0700

    mark test slow

--- test/ch/connect_test.exs
+++ test/ch/connect_test.exs
@@ -1,6 +1,7 @@
 defmodule Ch.ConnectTest do
   use ExUnit.Case, async: true
 
+  @tag :slow
   test "retries to connect even with exceptions" do
     {:ok, conn} =
       Ch.start_link(database: Ch.Test.database(), transport_opts: [sndbuf: nil])
