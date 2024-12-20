834ea16c992c4a0cb75c15084c3801799a3edaf5
ruslandoga <67764432+ruslandoga@users.noreply.github.com>
2024-12-20 21:08:32 +0700

    run benchmarks nightly

--- bench/insert.exs
+++ bench/insert.exs
@@ -1,54 +1,59 @@
-IO.puts("This benchmark is based on https://github.com/ClickHouse/clickhouse-go#benchmark\n")
+IO.puts("""
+This benchmark is based on https://github.com/ClickHouse/clickhouse-go#benchmark
+
+It tests how quickly a client can insert one million rows of the following schema:
+- col1 UInt64
+- col2 String
+- col3 Array(UInt8)
+- col4 DateTime
+""")
 
 port = String.to_integer(System.get_env("CH_PORT") || "8123")
 hostname = System.get_env("CH_HOSTNAME") || "localhost"
 scheme = System.get_env("CH_SCHEME") || "http"
 database = System.get_env("CH_DATABASE") || "ch_bench"
 
-{:ok, conn} = Ch.start_link(scheme: scheme, hostname: hostname, port: port)
-Ch.query!(conn, "CREATE DATABASE IF NOT EXISTS {$0:Identifier}", [database])
-
-Ch.query!(conn, """
-CREATE TABLE IF NOT EXISTS #{database}.benchmark (
-  col1 UInt64,
-  col2 String,
-  col3 Array(UInt8),
-  col4 DateTime
-) Engine Null
-""")
-
-types = [Ch.Types.u64(), Ch.Types.string(), Ch.Types.array(Ch.Types.u8()), Ch.Types.datetime()]
-statement = "INSERT INTO #{database}.benchmark FORMAT RowBinary"
+alias Ch.RowBinary
 
 rows = fn count ->
   Enum.map(1..count, fn i ->
-    [i, "Golang SQL database driver", [1, 2, 3, 4, 5, 6, 7, 8, 9], NaiveDateTime.utc_now()]
+    [i, "Golang SQL database driver", [1, 2, 3, 4, 5, 6, 7, 8, 9], DateTime.utc_now()]
   end)
 end
 
-alias Ch.RowBinary
+statement = "INSERT INTO #{database}.benchmark FORMAT RowBinary"
+types = ["UInt64", "String", "Array(UInt8)", "DateTime"]
 
 Benchee.run(
   %{
-    # "control" => fn rows -> Enum.each(rows, fn _row -> :ok end) end,
-    "encode" => fn rows -> RowBinary.encode_rows(rows, types) end,
-    "insert" => fn rows -> Ch.query!(conn, statement, rows, types: types) end,
-    # "control stream" => fn rows -> rows |> Stream.chunk_every(60_000) |> Stream.run() end,
-    "encode stream" => fn rows ->
-      rows
-      |> Stream.chunk_every(60_000)
-      |> Stream.map(fn chunk -> RowBinary.encode_rows(chunk, types) end)
-      |> Stream.run()
+    "Ch.query" => fn %{pool: pool, rows: rows} ->
+      Ch.query!(pool, statement, rows, types: types)
     end,
-    "insert stream" => fn rows ->
-      stream =
-        rows
-        |> Stream.chunk_every(60_000)
+    "Ch.stream" => fn %{pool: pool, rows: rows} ->
+      DBConnection.run(pool, fn conn ->
+        Stream.chunk_every(rows, 100_000)
         |> Stream.map(fn chunk -> RowBinary.encode_rows(chunk, types) end)
-
-      Ch.query!(conn, statement, stream, encode: false)
+        |> Stream.into(Ch.stream(conn, statement, [], encode: false))
+        |> Stream.run()
+      end)
     end
   },
+  before_scenario: fn rows ->
+    {:ok, pool} = Ch.start_link(scheme: scheme, hostname: hostname, port: port, pool_size: 1)
+
+    Ch.query!(pool, "CREATE DATABASE IF NOT EXISTS {$0:Identifier}", [database])
+
+    Ch.query!(pool, """
+    CREATE TABLE IF NOT EXISTS #{database}.benchmark (
+      col1 UInt64,
+      col2 String,
+      col3 Array(UInt8),
+      col4 DateTime
+    ) Engine Null
+    """)
+
+    %{pool: pool, rows: rows}
+  end,
   inputs: %{
     "1_000_000 rows" => rows.(1_000_000)
   }
