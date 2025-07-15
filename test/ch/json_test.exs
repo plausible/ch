eaf5c8205935d18bffa797d5dd8f0394678c4758
ruslandoga <ruslandoga+gh@icloud.com>
2025-07-15 21:05:42 +0300

    begin arrays

--- test/ch/json_test.exs
+++ test/ch/json_test.exs
@@ -21,10 +21,32 @@ defmodule Ch.JSONTest do
            ]
 
     assert Ch.query!(conn, ~s|select '{"a":42}'::json|).rows == [[%{"a" => 42}]]
-
     assert Ch.query!(conn, ~s|select '{}'::json|).rows == [[%{}]]
-
+    assert Ch.query!(conn, ~s|select '{"a":null}'::json|).rows == [[%{}]]
     assert Ch.query!(conn, ~s|select '{"a":3.14}'::json|).rows == [[%{"a" => 3.14}]]
+    assert Ch.query!(conn, ~s|select '{"a":true}'::json|).rows == [[%{"a" => true}]]
+    assert Ch.query!(conn, ~s|select '{"a":false}'::json|).rows == [[%{"a" => false}]]
+
+    assert Ch.query!(conn, ~s|select '{"a":{"b":"c"}}'::json|).rows == [
+             [%{"a.b" => "c"}]
+           ]
+
+    assert Ch.query!(conn, ~s|select '{"a":[]}'::json|).rows == [
+             [%{"a" => []}]
+           ]
+
+    assert Ch.query!(conn, ~s|select '{"a":[null]}'::json|).rows == [
+             [%{"a" => [nil]}]
+           ]
+
+    assert Ch.query!(conn, ~s|select '{"a":[1,3.14,"hello",null]}'::json|).rows == [
+             [%{"a" => ["1", "3.14", "hello", nil]}]
+           ]
+
+    # # <<31, 4, 35, 10, 35, 14, 35, 21, 48, 0, 128, 2, 16, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 10, 215, 163, 112, 61, 10, 1, 64, 0, 1, 115, 1, 1, 97, 21, 1, 98>>
+    # assert Ch.query!(conn, ~s|select '{"a":[1,2.13,"s",{"a":"b"}]}'::json|).rows == [
+    #          [%{"a.b" => 42}]
+    #        ]
   end
 
   # TODO
