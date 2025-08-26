d94146458c9b7114b371448723a884463c70b671
ruslandoga <ruslandoga+gh@icloud.com>
2025-08-26 14:39:27 +0900

    cleanup

--- lib/ch/types.ex
+++ lib/ch/types.ex
@@ -502,7 +502,7 @@ defmodule Ch.Types do
   defp named_columns_to_types([], acc), do: acc
 
   defp build_variant(types) do
-    Enum.sort_by(types, fn t -> t |> __MODULE__.encode() |> IO.iodata_to_binary() end)
+    Enum.sort_by(types, fn t -> IO.iodata_to_binary(encode(t)) end)
   end
 
   # TODO '', \'
