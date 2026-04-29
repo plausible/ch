faeac521e6c2de6d9b025994c48102c3ffb6b65e
github-action-benchmark <github@users.noreply.github.com>
2026-04-29 11:16:53 +0000

    add Ch RowBinary Encode (customBiggerIsBetter) benchmark result for b421389a003c9be36cff46c9a84a0f78b339b501

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1777375435148,
+  "lastUpdate": 1777461413462,
   "repoUrl": "https://github.com/plausible/ch",
   "entries": {
     "Ch RowBinary Encode": [
@@ -408,6 +408,42 @@ window.BENCHMARK_DATA = {
             "range": "stddev 3.02%",
             "unit": "ips",
             "extra": "average: 996.2 ms\nmedian: 977.27 ms"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "ruslandoga",
+            "username": "ruslandoga",
+            "email": "ruslandoga+gh@icloud.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "b421389a003c9be36cff46c9a84a0f78b339b501",
+          "message": "Update CHANGELOG to remove JSON support entries\n\nRemoved support for JSON type options and dynamic JSON encoding in RowBinary.",
+          "timestamp": "2026-04-23T22:27:39Z",
+          "url": "https://github.com/plausible/ch/commit/b421389a003c9be36cff46c9a84a0f78b339b501"
+        },
+        "date": 1777461413007,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Ch RowBinary Encode - RowBinary stream of 100k row chunks - 1_000_000 (UInt64, String, Array(UInt8), DateTime64(3, 'UTC'), DateTime) rows",
+            "value": 1.0478,
+            "range": "stddev 2.62%",
+            "unit": "ips",
+            "extra": "average: 954.37 ms\nmedian: 947.11 ms"
+          },
+          {
+            "name": "Ch RowBinary Encode - RowBinary - 1_000_000 (UInt64, String, Array(UInt8), DateTime64(3, 'UTC'), DateTime) rows",
+            "value": 1.0227,
+            "range": "stddev 17.93%",
+            "unit": "ips",
+            "extra": "average: 977.79 ms\nmedian: 1061.51 ms"
           }
         ]
       }
