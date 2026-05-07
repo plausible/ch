8be797722f0831ff9fa93290919dce776ef7ca9d
ruslandoga <ruslandoga+gh@icloud.com>
2026-05-07 20:31:03 +0300

    Backport scientific Decimal params to v0.4.3 (#337)
    * Backport scientific Decimal params to v0.4.3
    
    * add stream_data to dev env

--- test/ch/query_string_test.exs
+++ test/ch/query_string_test.exs
@@ -20,20 +20,4 @@ defmodule Ch.QueryStringTest do
     assert Ch.query!(conn, "select splitByChar('\t', {arg1:String})", %{"arg1" => "abc\t123"}).rows ==
              [[["abc", "123"]]]
   end
-
-  test "decimal params are bounded" do
-    query = Ch.Query.build("select {d:Decimal(76, 0)}")
-
-    {query_params, _headers, body} =
-      DBConnection.Query.encode(query, %{"d" => Decimal.new("1e1000000")}, [])
-
-    encoded =
-      case query_params do
-        [{"param_d", value}] -> value
-        [] -> IO.iodata_to_binary(body)
-      end
-
-    assert encoded =~ "1E+1000000"
-    assert byte_size(encoded) < 300
-  end
 end
