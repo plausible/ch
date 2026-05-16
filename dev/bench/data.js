e11e4bde1f4e035dad50abedc2621ff5cccd2431
github-action-benchmark <github@users.noreply.github.com>
2026-05-16 10:47:41 +0000

    add Ch RowBinary Encode (customBiggerIsBetter) benchmark result for 25bf2a4b55f638f3a79ff5a977b84b1b76f8fd31

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1778844905340,
+  "lastUpdate": 1778928461208,
   "repoUrl": "https://github.com/plausible/ch",
   "entries": {
     "Ch RowBinary Encode": [
@@ -1628,6 +1628,42 @@ window.BENCHMARK_DATA = {
             "range": "stddev 17.7%",
             "unit": "ips",
             "extra": "average: 1023.1 ms\nmedian: 1103.06 ms"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "ruslandoga",
+            "username": "ruslandoga",
+            "email": "ruslandoga+gh@icloud.com"
+          },
+          "committer": {
+            "name": "ruslandoga",
+            "username": "ruslandoga",
+            "email": "ruslandoga+gh@icloud.com"
+          },
+          "id": "25bf2a4b55f638f3a79ff5a977b84b1b76f8fd31",
+          "message": "release v0.8.3",
+          "timestamp": "2026-05-12T12:03:25Z",
+          "url": "https://github.com/plausible/ch/commit/25bf2a4b55f638f3a79ff5a977b84b1b76f8fd31"
+        },
+        "date": 1778928460322,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Ch RowBinary Encode - RowBinary stream of 100k row chunks - 1_000_000 (UInt64, String, Array(UInt8), DateTime64(3, 'UTC'), DateTime) rows",
+            "value": 1.0351,
+            "range": "stddev 2.0%",
+            "unit": "ips",
+            "extra": "average: 966.09 ms\nmedian: 962.04 ms"
+          },
+          {
+            "name": "Ch RowBinary Encode - RowBinary - 1_000_000 (UInt64, String, Array(UInt8), DateTime64(3, 'UTC'), DateTime) rows",
+            "value": 1.0074,
+            "range": "stddev 18.2%",
+            "unit": "ips",
+            "extra": "average: 992.61 ms\nmedian: 1080.03 ms"
           }
         ]
       }
