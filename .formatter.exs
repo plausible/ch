75cc6ff103ac680d0681aaedbd16fbf34eacb215
ruslandoga <ruslandoga+gh@icloud.com>
2026-07-29 13:01:09 +0300

    rm bench

--- .formatter.exs
+++ .formatter.exs
@@ -1,5 +1,5 @@
 # Used by "mix format"
 [
-  inputs: ["{mix,.formatter}.exs", "{config,lib,test,bench,dev}/**/*.{ex,exs}"],
+  inputs: ["{mix,.formatter}.exs", "{lib,test}/**/*.{ex,exs}"],
   import_deps: [:stream_data]
 ]
